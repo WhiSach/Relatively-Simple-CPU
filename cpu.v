9cac940f66be7527dcaaf2dfbd82604336665240
WhiSach <47545327+WhiSach@users.noreply.github.com>
2026-06-11 20:11:30 -0400

    add cpu.v file

---
+++ cpu.v
@@ -0,0 +1,10 @@
+module cpu (
+    input wire clk,
+    input wire rst,
+    // Memory Interface
+    input wire [7:0] mem_data_in,   // Data read from RAM
+    output reg [15:0] mem_addr,     // Address sent to RAM
+    output reg [7:0] mem_data_out,  // Data written to RAM
+    output reg mem_rd,              // Memory read enable
+    output reg mem_wr               // Memory write enable
+);
